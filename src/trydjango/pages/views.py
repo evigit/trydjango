7edb8b13304670758584db372ea3c94f2950bc16
meowpeow <45047292+meowpeow@users.noreply.github.com>
2019-03-05 02:09:25 +0300

    First commit

---
+++ src/trydjango/pages/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(*args, **kwargs): # *args, **kwargs
+	return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+
+
+def contact_view(*args, **kwargs): # *args, **kwargs
+	return HttpResponse("<h1>Contact Page</h1>")
+
+
+def about_view(*args, **kwargs): # *args, **kwargs
+	return HttpResponse("<h1>About Page</h1>")
+
+
+def social_view(*args, **kwargs): # *args, **kwargs
+	return HttpResponse("<h1>Social Page</h1>")
