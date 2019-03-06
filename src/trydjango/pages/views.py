fc4a01201ebc15bb9cb818fff59f7a64cb906bb7
meowpeow <45047292+meowpeow@users.noreply.github.com>
2019-03-06 22:54:55 +0300

    Built-in tags
    different types of built-in tags was tested

--- src/trydjango/pages/views.py
+++ src/trydjango/pages/views.py
@@ -14,9 +14,11 @@ def contact_view(request, *args, **kwargs): # *args, **kwargs
 
 def about_view(request, *args, **kwargs): # *args, **kwargs
 	my_context = {
-		"my_text": "This is about us",
+		"title": "This is about us",
+		"this_is_true": True,
 		"my_number": 123,
-		'my_list': [777,888,999]
+		'my_list': [1313,4231,312, "Abc"],
+		'my_html': "<h1>Hello World</h1>"
 	}
 	return render(request, "about.html", my_context)
 
